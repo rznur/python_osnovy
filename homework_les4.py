893ee3ae7a3d8d464daa5e289d7b36a7c47e9910
rznur <roza.nursaidova@gmail.com>
2019-02-25 14:32:04 +0600

    les4

---
+++ homework_les4.py
@@ -0,0 +1,390 @@
+
+# coding: utf-8
+
+# ## LESSON 4 EASY
+
+# ## #1
+
+# In[1]:
+
+
+# Все задачи текущего блока решите с помощью генераторов списков!
+# e.g. : a = [i for i in range(1,15)]
+
+
+# Задание-1:
+# Дан список, заполненный произвольными целыми числами. 
+# Получить новый список, элементы которого будут
+# квадратами элементов исходного списка
+# [1, 2, 4, 0] --> [1, 4, 16, 0]
+
+orig_list = [1, 2, 4, 0]
+squar_list = [i**2 for i in orig_list]
+
+squar_list## #1
+
+
+# ## #2
+
+# In[2]:
+
+
+
+# Задание-2:
+# Даны два списка фруктов.
+# Получить список фруктов, присутствующих в обоих исходных списках.
+
+fruits1 = ['apple', 'banana', 'grapes', 'lemon', 'kiwi']
+fruits2 = ['grapes', 'pomegranade', 'watermelon', 'apple', 'lemon']
+
+
+joint_list = [x for x in fruits1 if x in fruits2]
+joint_list
+
+
+# Черновик
+
+# In[ ]:
+
+
+#l3 = [x for x in l1 if x not in l2]
+
+
+# In[3]:
+
+
+joint_list = []
+for i in fruits1:
+    for g in fruits2:
+        if i == g:
+            joint_list.append(i)
+joint_list
+
+
+# In[ ]:
+
+
+# Задание-3:
+# Дан список, заполненный произвольными числами.
+# Получить список из элементов исходного, удовлетворяющих следующим условиям:
+# + Элемент кратен 3
+# + Элемент положительный
+# + Элемент не кратен 4
+
+
+# In[4]:
+
+
+list1 = [2, 3, 6, -9, 16, 15,36, -24, 24]
+
+fin_list =  [x for x in list1 if x%3 == 0 and x>=0 and x%4!=0]
+fin_list
+
+
+# ## LESSON 4 NORMAL
+
+# ## #1
+
+# ## with re
+
+# In[5]:
+
+
+# Задание-1:
+# Вывести символы в нижнем регистре, которые находятся вокруг
+# 1 или более символов в верхнем регистре.
+# Т.е. из строки "mtMmEZUOmcq" нужно получить ['mt', 'm', 'mcq']
+# Решить задачу двумя способами: с помощью r e и без.
+import re
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+pattern = '[a-z]+'
+
+new_list = []
+for match in re.finditer(pattern, line):
+    g = match.group()
+    new_list.append(g)
+print(new_list)
+
+
+# In[6]:
+
+
+#another way with list comprehensions
+import re
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+pattern = '[a-z]+'
+
+new_list = []
+new_list.append([match.group() for match in re.finditer(pattern,line)])
+print(new_list)
+
+
+# In[7]:
+
+
+# another way using re, but doesn't generate list
+
+import re
+
+line = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+pattern = '[a-z]+'
+
+new_list = []
+for match in re.finditer(pattern, line):
+    s = match.start()
+    e = match.end()
+    print(line[s:e])
+
+
+# ## Solution without re
+
+# In[8]:
+
+
+# reshila bez re...no zamknulo na string i list...convertnula str v list i tolko tak reshila kak list, pravda po primeru s stackoverflow
+
+line = 'DUMmtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysmNO'       'GIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewVzK'       'TUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSAHqn'       'LxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIVjXa'       'pzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnWete'       'kUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfCvzQ'       'WrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbRuXb'       'JrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkmjCC'       'EUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOnLfB'       'tQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGSeuT'       'SkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCfKCu'       'UJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWHuXB'       'qHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQNJFa'       'XiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQoiQ'       'zTYwZAiRwycdlHfyHNGmkNqSwXUrxGc'
+
+import itertools as it
+
+line_ls = [line]
+
+lis = [[''.join(v) for k,v in it.groupby(x,key = lambda z:z.islower())] for x in line_ls]
+lower_output = []
+
+if lis[0][0].islower():
+    lower_output.append(lis[0][::2])
+else:
+    lower_output.append(lis[0][1::2])
+    
+lower_output = lower_output[0]
+lower_output
+
+
+# ## #2
+
+# In[9]:
+
+
+# Задание-2:
+# Вывести символы в верхнем регистре, слева от которых находятся
+# два символа в нижнем регистре, а справа - два символа в верхнем регистре.
+# Т.е. из строки 
+# "GAMkgAYEOmHBSQsSUHKvSfbmxULaysmNOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLec"
+# нужно получить список строк: ['AY', 'NOGI', 'P']
+# Решить задачу двумя способами: с помощью re и без.
+
+line_2 = 'mtMmEZUOmcqWiryMQhhTxqKdSTKCYEJlEZCsGAMkgAYEOmHBSQsSUHKvSfbmxULaysm'       'NOGIPHpEMujalpPLNzRWXfwHQqwksrFeipEUlTLeclMwAoktKlfUBJHPsnawvjPhfgewV'       'fzKTUfSYtBydXaVIpxWjNKgXANvIoumesCSSvjEGRJosUfuhRRDUuTQwLlJJJDdkVjfSA'       'HqnLxooisBDWuxIhyjJaXDYwdoVPnsllMngNlmkpYOlqXEFIxPqqqgAWdJsOvqppOfyIV'       'jXapzGOrfinzzsNMtBIOclwbfRzytmDgEFUzxvZGkdOaQYLVBfsGSAfJMchgBWAsGnBnW'       'etekUTVuPluKRMQsdelzBgLzuwiimqkFKpyQRzOUyHkXRkdyIEBvTjdByCfkVIAQaAbfC'       'vzQWrMMsYpLtdqRltXPqcSMXJIvlBzKoQnSwPFkapxGqnZCVFfKRLUIGBLOwhchWCdJbR'       'uXbJrwTRNyAxDctszKjSnndaFkcBZmJZWjUeYMdevHhBJMBSShDqbjAuDGTTrSXZywYkm'       'jCCEUZShGofaFpuespaZWLFNIsOqsIRLexWqTXsOaScgnsUKsJxiihwsCdBViEQBHQaOn'       'LfBtQQShTYHFqrvpVFiiEFMcIFTrTkIBpGUflwTvAzMUtmSQQZGHlmQKJndiAXbIzVkGS'       'euTSkyjIGsiWLALHUCsnQtiOtrbQOQunurZgHFiZjWtZCEXZCnZjLeMiFlxnPkqfJFbCf'       'KCuUJmGYJZPpRBFNLkqigxFkrRAppYRXeSCBxbGvqHmlsSZMWSVQyzenWoGxyGPvbnhWH'       'uXBqHFjvihuNGEEFsfnMXTfptvIOlhKhyYwxLnqOsBdGvnuyEZIheApQGOXWeXoLWiDQN'       'JFaXiUWgsKQrDOeZoNlZNRvHnLgCmysUeKnVJXPFIzvdDyleXylnKBfLCjLHntltignbQ'       'oiQzTYwZAiRwycdlHfyHNGmkNqSwXUrxGC'
+import re
+
+pattern ='(?<=[a-z]{2})[A-Z]+(?=[A-Z]{2})'
+
+re.findall(pattern,line_2)
+    
+
+
+# In[10]:
+
+
+import itertools as it
+
+line_ls = [line_2]
+
+lis = [[''.join(v) for k,v in it.groupby(x,key = lambda z:z.islower())] for x in line_ls][0]
+
+output = []
+for i in range(len(lis)):
+    if len(lis[i])>2 and lis[i].isupper() and len(lis[i-1])>1 and lis[i-1][-2:].islower():
+        output.append(lis[i][:-2])
+output
+#lis[3][-2:]#.islower()
+
+
+# Черновик
+
+# In[11]:
+
+
+import re
+
+test='abcKULOPlkOPDDklknuiyTU'
+
+#pattern = '(?<=abc)DEF'
+pattern ='(?<=[a-z]{2})[A-Z]+(?=[A-Z]{2})'
+re.findall(pattern,test)
+
+
+# ## #3
+
+# # Уточнить, как вкл. 2500-зн. число. с такими числами, комп виснет
+
+# In[15]:
+
+
+# NE POLUCHAETSA s bolshimi chislami!DORESHAT
+# Задание-3:
+# Напишите скрипт, заполняющий указанный файл (самостоятельно задайте имя файла)
+# произвольными целыми цифрами, в результате в файле должно быть
+# 2500-значное произвольное число.
+# Найдите и выведите самую длинную последовательность одинаковых цифр
+# в вышезаполненном файле.
+
+import random
+
+random_numbers = open('ran_numbers.txt', 'w')
+
+qty_numbers =  100 #выделила отдельно, чтобы легче делать сверку/отладку
+
+from random import randint
+from itertools import groupby
+
+for count in range (qty_numbers):
+    number = random.randint(10,100)    
+    random_numbers.write(str(number)+ '\n')
+# check file
+#file = open('ran_numbers.txt', 'r') 
+#data = [file.read()]
+#data
+rndm_list = []
+
+with open('ran_numbers.txt', 'r') as myfile:
+    data = myfile.read().split('\n')
+    data.remove('')
+    for num in data:
+        rndm_list.append(int(num))  
+
+print(rndm_list)
+
+
+out = max((list(g) for k, g in groupby(rndm_list)), key=len)
+
+out
+
+
+# ## LESSON 4 HARD
+
+# ## #1
+
+# In[16]:
+
+
+# Задание-1:
+# Матрицы в питоне реализуются в виде вложенных списков:
+# Пример. Дано:
+matrix = [[1, 0, 8],
+          [3, 4, 1],
+          [0, 4, 2]]
+          
+# Выполнить поворот (транспонирование) матрицы
+# Пример. Результат:
+# matrix_rotate = [[1, 3, 0],
+#                  [0, 4, 4],
+#                  [8, 1, 2]]
+
+# Суть сложности hard: Решите задачу в одну строку
+
+import numpy as np
+
+matrix_rotate = np.transpose(matrix)
+
+matrix_rotate
+
+
+# In[17]:
+
+
+#alternative without libraries
+
+[[row[i] for row in matrix] for i in range(len(matrix[0]))]
+
+
+# ## #2
+
+# In[18]:
+
+
+# Задание-2:
+# Найдите наибольшее произведение пяти последовательных цифр в 1000-значном числе.
+# Выведите произведение и индекс смещения первого числа последовательных 5-ти цифр.
+# Пример 1000-значного числа:
+number = """
+73167176531330624919225119674426574742355349194934
+96983520312774506326239578318016984801869478851843
+85861560789112949495459501737958331952853208805511
+12540698747158523863050715693290963295227443043557
+66896648950445244523161731856403098711121722383113
+62229893423380308135336276614282806444486645238749
+30358907296290491560440772390713810515859307960866
+70172427121883998797908792274921901699720888093776
+65727333001053367881220235421809751254540594752243
+52584907711670556013604839586446706324415722155397
+53697817977846174064955149290862569321978468622482
+83972241375657056057490261407972968652414535100474
+82166370484403199890008895243450658541227588666881
+16427171479924442928230863465674813919123162824586
+17866458359124566529476545682848912883142607690042
+24219022671055626321111109370544217506941658960408
+07198403850962455444362981230987879927244284909188
+84580156166097919133875499200524063689912560717606
+05886116467109405077541002256983155200055935729725
+71636269561882670428252483600823257530420752963450"""
+
+
+# In[19]:
+
+
+value = int(number.replace('\n',''))
+smax = 0
+for j in range(0,len(number.replace('\n',''))-5):
+    s = 1
+    for jj in range(j,j+5):
+        s = s*int(number.replace('\n','')[jj])
+        if s>smax:
+            smax = s
+smax
+
+
+# ## #3
+
+# In[ ]:
+
+
+
+# Задание-3 (Ферзи):
+# Известно, что на доске 8×8 можно расставить 8 ферзей так, чтобы они не били
+# друг друга. Вам дана расстановка 8 ферзей на доске.
+# Определите, есть ли среди них пара бьющих друг друга.
+# Программа получает на вход восемь пар чисел,
+# каждое число от 1 до 8 — координаты 8 ферзей.
+# Если ферзи не бьют друг друга, выведите слово NO, иначе выведите YES.
+
+
+# In[20]:
+
+
+# я не очень разбираюсь в шахматах, так что пришлось воспользоваться помощью сетки ;) 
+
+n = 8
+x = []
+y = []
+for i in range(n):
+    new_x, new_y = [int(s) for s in input('введите координаты восьми ферзей в формате "x,y" ').split(',')]
+    x.append(new_x)
+    y.append(new_y)
+ 
+correct = True
+for i in range(n):
+    for j in range(i + 1, n):
+        if x[i] == x[j] or y[i] == y[j] or abs(x[i] - x[j]) == abs(y[i] - y[j]):
+            correct = False
+ 
+if correct:
+    print('NO')
+else:
+    print('YES')
+
